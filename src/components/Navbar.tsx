9480e79cb4abe60f0e576a1aacffdac12fa4ff7c
Adnan <adnaniqbal602@mail.com>
2023-07-06 19:31:03 +0500

    create table

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import Avatar from '@mui/material/Avatar';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Button from '@mui/material/Button';
@@ -20,7 +14,7 @@ interface MenuAppBarProps {
     drawerWidth: number;
 }
 
-export default function MenuAppBar({handleDrawerToggle, drawerWidth}: MenuAppBarProps) {
+export default function NavBar({handleDrawerToggle, drawerWidth}: MenuAppBarProps) {
   
 
   return (
